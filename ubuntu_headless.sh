c1dc033f2fc42d4a4e40de009fd9eabf1382f7fb
bing-deng <804433988@qq.com>
2021-08-27 22:15:00 +0900

    Create ubuntu_headless.sh

---
+++ ubuntu_headless.sh
@@ -0,0 +1,8 @@
+
+sudo apt-get update && sudo apt-get install -y libappindicator1 fonts-liberation && apt-get --fix-broken install
+cd /home && mkdir chrome-headless && cd  chrome-headless
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip && unzip chromedriver_linux64.zip
+apt --fix-broken install && apt install unzip
+sudo apt install python3-pip && pip3 install selenium
