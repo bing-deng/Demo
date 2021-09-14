11041ea73e3c9eee4ee02494f34c267667d358af
bing-deng <804433988@qq.com>
2021-09-14 16:00:59 +0900

    Update ubuntu_headless.sh

--- ubuntu_headless.sh
+++ ubuntu_headless.sh
@@ -3,8 +3,9 @@ sudo apt-get update && sudo apt-get install -y libappindicator1 fonts-liberation
 cd /home && mkdir chrome-headless && cd  chrome-headless
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+sudo apt install unzip
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip && unzip chromedriver_linux64.zip
-apt --fix-broken install && apt install unzip
+apt --fix-broken install 
 sudo apt install python3-pip && pip3 install selenium
 cd /home/chrome-headless && unzip chromedriver_linux64.zip
 #sh -c "$(curl -sSL https://raw.githubusercontent.com/bing-deng/Demo/master/ubuntu_headless.sh)"
