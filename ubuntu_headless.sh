1d15f873bd3b71a277ed62af89c70fd63d9c4d80
bing-deng <804433988@qq.com>
2021-10-28 12:11:19 +0900

    Update ubuntu_headless.sh

--- ubuntu_headless.sh
+++ ubuntu_headless.sh
@@ -1,4 +1,4 @@
-
+sudo apt install unzip
 sudo apt-get update && sudo apt-get install -y libappindicator1 fonts-liberation && apt-get --fix-broken install
 cd /home && mkdir chrome-headless && cd  chrome-headless
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
